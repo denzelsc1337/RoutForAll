c935c1b09a8b41f678cf036b7d1756c101acfc68
Arroshi <victor.arroyo0302@gmail.com>
2021-10-31 14:27:27 -0500

    .

---
+++ View/Vehiculo/AddVehiculos.php
@@ -0,0 +1,26 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Agregar Clientes</title>
+
+    <link rel="stylesheet" href="../css/main.css">
+    
+</head>
+
+<body>
+    <div class="header">
+        <nav class="navigation">
+            <a class="inner-shadow active" href="../../index.php">Home</a>
+            <a class="outer-shadow hover-in-shadow" href="../AddRoute.php">Asignar Routes</a>
+            <?php include("../../View/Header/mainHeader.php"); ?>
+
+        </nav>
+    </div>
+
+    <section class="sec_container">
+    </section>
+</body>
