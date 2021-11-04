4dfe149e219d2bf21104ad25b07ade1bfffb9501
Arroshi <victor.arroyo0302@gmail.com>
2021-11-04 09:56:35 -0500

    .

--- View/AddRoute.php
+++ View/AddRoute.php
@@ -13,6 +13,10 @@ include_once('../config/connection.php');
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/corona/app.0d92b70a.css">
+    <link rel="stylesheet" href="css/corona/chunk-4ab62850.48f556ca.css">
+    <link rel="stylesheet" href="css/corona/chunk-162ef2da.0e433876.css">
+    <link rel="stylesheet" href="css/corona/chunk-vendors.0dbf83be.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
     <title>Testeo</title>
 
@@ -66,7 +70,7 @@ include_once('../config/connection.php');
         <?php  
         echo 'Versión actual de PHP: ' . phpversion();
         ?>
-        <table border="1px" class="table_">
+        <table  class="table table_ b-table">
             <?php
             require_once('../Controller/controllerList.php');
             ?>
