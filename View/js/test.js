544bd0c3f8ca393cc34eff49ff2ef84db321f99e
Arroshi <victor.arroyo0302@gmail.com>
2021-11-12 10:56:33 -0500

    .

--- View/js/test.js
+++ View/js/test.js
@@ -27,10 +27,10 @@
     targets.forEach(target => {
         target.addEventListener('click', () => {
             content.forEach(c => {
-                c.classList.remove('active')
+                c.classList.remove("active")
             })
             const t = document.querySelector(target.dataset.target)
-            t.classList.add('active');
+            t.classList.add("active");
         })
     })
 
