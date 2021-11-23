96f14ba619b0e1b0150a958a2bb3556e508acbcf
Arroshi <victor.arroyo0302@gmail.com>
2021-11-23 17:21:46 -0500

    .

--- View/Cliente/AddClientes.php
+++ View/Cliente/AddClientes.php
@@ -8,6 +8,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/corona/app.0d92b70a.css">
     <link rel="stylesheet" href="../css/corona/chunk-4ab62850.48f556ca.css">
     <link rel="stylesheet" href="../css/corona/chunk-162ef2da.0e433876.css">
@@ -30,7 +31,10 @@
         <p data-target="#uno" class="tabs-item active">Mostrar</p>
         <p data-target="#dos" class="tabs-item">Agregar</p>
         <p data-target="#tres" class="tabs-item">Tab 3</p>
+
     </div>
+
+
 
 
     <div class="tab-sub-content">
@@ -64,33 +68,19 @@
                             <td><?php echo $listClients["telefono"]; ?></td>
                             <td><?php echo $listClients["celular"]; ?></td>
                              <td>
-                                <button type="button" class="btn btn-success btnAsign" data-toggle="modal" data-target="exampleModal">Edit</button>
+                                <button type="button" class="btn btn-success btnAsign" data-bs-toggle="modal" data-bs-target="#asdasdasd">Edit</button>
+                                <!-- <button type="button" class="btn btn-success btnAsign" data-toggle="modal" data-target="asdasdasd">Edit</button> -->
                              </td>
                         </tr>
                 </tbody>
             <?php } ?>
 
             </table>
+
         </div>
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                ...
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-              </div>
-            </div>
-          </div>
-        </div>
+        
+
+       
 
         <div data-content id="dos" class="sub-content-item">
 
@@ -189,6 +179,44 @@
     </div>
 
 
+    <!-- <div class="modal fade" id="asdasdasd" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                ...
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div> -->
+
+<!-- Modal -->
+<div class="modal fade" id="asdasdasd" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 
@@ -198,6 +226,7 @@
 
     <!--  -->
     <script src="../js/test.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
     <!--  -->
 
     <script>
@@ -226,6 +255,11 @@
             console.log("testing");
         }*/
     </script>
+
+
+
+
+
 
 </body>
 
