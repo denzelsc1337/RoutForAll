2a0f42f919ad40b448c47c7dff4d5f2985450f13
denzelsc1337 <denzelsotomayor@gmail.com>
2021-11-23 23:20:58 -0500

    Update AddClientes.php

--- View/Cliente/AddClientes.php
+++ View/Cliente/AddClientes.php
@@ -199,24 +199,68 @@
           </div>
         </div> -->
 
-<!-- Modal -->
-<div class="modal fade" id="asdasdasd" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+            <!-- Modal -->
+            <div class="modal fade" id="asdasdasd" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h2 class="modal-title" id="exampleModalLabel">Editar</h2>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    <form>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label>RUC</label>
+                                <input type="text" class="form-control" id="rucUpdt">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label>Razon Social</label>
+                                <input type="text" class="form-control" id="rucUpdt">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="custom-control custom-radio custom-control-inline">
+                              <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+                              <label class="custom-control-label" for="customRadioInline1">Natural</label>
+                            </div>
+                            <div class="custom-control custom-radio custom-control-inline">
+                              <input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input">
+                              <label class="custom-control-label" for="customRadioInline2">Juridica</label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputAddress">Address</label>
+                            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputAddress">Correo</label>
+                            <input type="text" class="form-control" id="inputAddress" placeholder="Email">
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label>Telefono</label>
+                                <input type="text" class="form-control" id="rucUpdt">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label>Celular</label>
+                                <input type="text" class="form-control" id="rucUpdt">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputAddress">Contacto</label>
+                            <input type="text" class="form-control" id="inputAddress" placeholder="Alberto">
+                        </div>
+
+                    </form>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                  </div>
+                </div>
+              </div>
+            </div>
 
 
 
