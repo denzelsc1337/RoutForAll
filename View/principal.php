4840e20a1cc9b42e48040f4bd3459b666b6cbdad
Arroshi <victor.arroyo0302@gmail.com>
2021-12-10 00:31:52 -0500

    .

--- View/principal.php
+++ View/principal.php
@@ -1,7 +1,7 @@
 <?php
 //include_once('config/Connection.php');
 require_once('../config/security.php');
-include_once ((dirname(__FILE__) . '../../config/connection.php'));
+include_once((dirname(__FILE__) . '../../config/connection.php'));
 // echo "<br>";
 // echo $_SERVER['HTTP_USER_AGENT'];
 // echo "<br>";
@@ -20,6 +20,7 @@ include_once ((dirname(__FILE__) . '../../config/connection.php'));
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
     <!--  -->
     <!-- <link rel="stylesheet" href="css/corona/app.0d92b70a.css">
     <link rel="stylesheet" href="css/corona/chunk-4ab62850.48f556ca.css">
@@ -33,21 +34,29 @@ include_once ((dirname(__FILE__) . '../../config/connection.php'));
 
     <div class="header">
         <nav class="navigation">
-            <a class="a_cont" href="principal.php">Home</a>
-            <a class="a_cont" href="AddRoute.php">Rutas</a>
-            <a class="a_cont" href="Cliente/AddClientes.php">Clientes</a>
-            <a class="a_cont" href="Carga/AddCargas.php">Cargas</a>
-            <a class="a_cont" href="Vehiculo/AddVehiculos.php">Vehiculo</a>
-            <a class="a_cont" href="Conductor/AddConductor.php">Conductor</a>
-            <a class="a_cont" href="Usuarios/AddUser.php">Usuarios</a>
-            <a class="a_cont" href="Usuarios/AddUser.php">Usuarios</a>
-            <a href="../config/logout.php" class="Blogger">Cerrar Sesión <i class="fa fa-power-off"></i></a>
+            <div>
+                <a class="a_cont" href="principal.php">Home</a>
+                <a class="a_cont" href="AddRoute.php">Rutas</a>
+                <a class="a_cont" href="Cliente/AddClientes.php">Clientes</a>
+                <a class="a_cont" href="Carga/AddCargas.php">Cargas</a>
+                <a class="a_cont" href="Vehiculo/AddVehiculos.php">Vehiculo</a>
+                <a class="a_cont" href="Conductor/AddConductor.php">Conductor</a>
+                <a class="a_cont" href="Usuarios/AddUser.php">Usuarios</a>
+                <!-- <a class="a_cont" href="Usuarios/AddUser.php">Usuarios</a> -->
+                <!-- <a href="../config/logout.php" class="Blogger">Cerrar Sesión <i class="fa fa-power-off"></i></a> -->
+            </div>
+            <div class="logout">
+                <!-- <i class="fas fa-sort-down"></i> -->
+
+                <p class="Blogger">
+                    ¡Bienvenido
+                    <?php echo $_SESSION['user'] ?>¡&nbsp;<i class="bi bi-person-fill"></i>
+                </p>
+                <a href="../config/logout.php" class="Blogger">Cerrar Sesión <i class="fa fa-power-off"></i></a>
+            </div>
         </nav>
     </div>
-        <p class="Blogger">
-        ¡Bienvenido 
-            <?php echo $_SESSION['user'] ?><i class="bi bi-person-fill"></i>
-        </p>
+
     <section class="sec_container">
         <div id="googleMap" style="width:100%;height:750px;"></div>
     </section>
@@ -66,5 +75,5 @@ include_once ((dirname(__FILE__) . '../../config/connection.php'));
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBbFjupaAAf9ne0JlXq-B48RHwppD7LDDA&callback=theMap"></script>
 
 <!--  -->
-<script src="View/js/test.js"></script>
+<script src="../View/js/test.js"></script>
 <!--  -->
