4e921a6929d128dc166bee2937f38b044ed8df98
Arroshi <victor.arroyo0302@gmail.com>
2021-12-20 01:21:55 -0500

    .

--- View/AddRoute.php
+++ View/AddRoute.php
@@ -198,7 +198,7 @@ $tourresult = $num_result->fetch_array()['pesoCarga'] ?? '';*/
 
     <!-- Modal -->
     <div class="modal fade" id="editRoute" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Asignar Rutas a:</h5>
